c3e342bcd9e4cae792f10d6cf198a06670f1e4b2
heike <hofmann@iastate.edu>
2019-07-26 07:51:26 -0700

    r code for interactive plots

---
+++ 3.1-interactive-plots.r
@@ -0,0 +1,285 @@
+## ----echo = FALSE--------------------------------------------------------
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE,
+  collapse = TRUE,
+  comment = "#>",
+  fig.height = 4,
+  fig.width = 8,
+  fig.align = "center",
+  cache = FALSE
+)
+
+
+## /* custom.css */
+
+## .left-code {
+
+##   color: #777;
+
+##   width: 48%;
+
+##   height: 92%;
+
+##   float: left;
+
+## }
+
+## .right-plot {
+
+##   width: 50%;
+
+##   float: right;
+
+##   padding-left: 1%;
+
+## }
+
+## ----echo=FALSE----------------------------------------------------------
+library(tidyverse)
+library(plotly)
+library(gganimate)
+library(datasauRus)
+
+
+## ---- echo=FALSE, fig.width=10, fig.height = 8---------------------------
+interactive <- read_csv("../data/package-info-Jul-2019.csv")
+inter.summary <- interactive %>%
+  group_by(package) %>%
+  summarise(maxdown = max(downloads),
+            maxtime = start[which.max(downloads)])
+
+interactive %>% 
+  filter(start < lubridate::ymd("2019-07-01")) %>%
+  ggplot(aes(x = start, y = downloads, colour=package)) +
+  geom_line() +
+  theme_bw() +
+  theme(legend.position="none") +
+  geom_text(
+    aes(x = maxtime, y = 1.05*maxdown, label=package),
+    data = inter.summary %>% filter(maxdown > 10000)) +
+  ylab("Monthly downloads") +
+  xlab("Time")
+
+
+
+
+## ----eval=FALSE----------------------------------------------------------
+## library(ggvis)
+## data("economics", package = "ggplot2")
+## 
+## ggvis(economics, x=~date, y=~psavert)
+
+
+## ----eval=FALSE----------------------------------------------------------
+## economics %>%
+##   ggvis(~date, ~psavert) %>%
+##   layer_points() %>%
+##   layer_smooths()
+
+
+## ----eval=FALSE----------------------------------------------------------
+## economics %>%
+##   ggvis(~date, ~psavert) %>%
+##   layer_points(opacity := 0.2) %>%
+##   layer_smooths(
+##     span = input_slider(0.2, 0.5),
+##     stroke := "red"
+##   )
+
+
+## ---- echo =FALSE, eval=FALSE--------------------------------------------
+## economics %>%
+##   ggvis(~date, ~psavert) %>%
+##   layer_points(
+##     opacity := input_slider(0,1, label="opacity")) %>%
+##   layer_smooths(
+##     span = input_slider(0.02, 0.5, label="span"),
+##     stroke := "red"
+##   )
+
+
+## ----eval=FALSE----------------------------------------------------------
+## all_values <- function(x) {
+##   if (is.null(x)) return(NULL)
+##   paste0(names(x), ": ", format(x), collapse = "<br />")
+## }
+## 
+## economics %>%
+##   ggvis(~unemploy, ~psavert) %>%
+##   layer_points() %>%
+##   add_tooltip(all_values, "hover")
+
+
+## ----echo=FALSE, eval=FALSE----------------------------------------------
+## all_values <- function(x) {
+##   if (is.null(x)) return(NULL)
+##   paste0(names(x), ": ", format(x), collapse = "<br />")
+## }
+## 
+## economics %>%
+##   ggvis(~unemploy, ~psavert) %>%
+##   layer_points() %>%
+##   add_tooltip(all_values, "hover")
+
+
+## ----eval=FALSE----------------------------------------------------------
+## model_type <- input_checkbox(label = "Use loess curve",
+##   map = function(val) if(val) "loess" else "lm")
+## economics %>%
+##   ggvis(~date, ~psavert) %>%
+##   layer_points() %>%
+##     layer_model_predictions(
+##         model = model_type)
+
+
+## ----eval=FALSE----------------------------------------------------------
+## library(plotly)
+## plot_ly(data = economics, x = ~date, y = ~unemploy / pop)
+
+
+## ----echo=FALSE----------------------------------------------------------
+library(plotly)
+plot_ly(data = economics, x = ~date, y = ~unemploy / pop)
+
+
+## ------------------------------------------------------------------------
+ggplot(data=economics, aes(x = date, y = unemploy / pop)) +  
+        geom_point() + geom_line()
+
+
+## ------------------------------------------------------------------------
+ggplotly()
+
+
+## ----fig.width=6, fig.height=6-------------------------------------------
+library(GGally)
+p <- ggpairs(economics[,3:6])
+ggplotly(p)
+
+
+## ------------------------------------------------------------------------
+data(canada.cities, package = "maps")
+viz <- ggplot(canada.cities, aes(long, lat)) +
+  borders(regions = "canada") +
+  coord_equal() +
+  geom_point(aes(text = name, size = log2(pop)), colour = "red", alpha = 1/4)
+ ggplotly(viz)
+
+
+## ----eval=FALSE----------------------------------------------------------
+## #devtools::install_github("ropenscilabs/eechidna")
+## library(eechidna)
+## launch_app(age = c("Age20_24", "Age85plus"),
+##   religion = c("Christianity", "Catholic", "NoReligion"),
+##   other = c("Unemployed", "AusCitizen", "MedianPersonalIncome")
+## )
+
+
+## ---- echo=FALSE, fig.width = 8, fig.height = 6--------------------------
+library(gapminder)
+
+ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, colour = country)) +
+  geom_point(alpha = 0.7) +
+  scale_colour_manual(values = country_colors, guide=FALSE) +
+  scale_size("Population size", range = c(2, 12), breaks=c(1*10^8, 2*10^8, 5*10^8, 10^9, 2*20^9)) +
+  scale_x_log10() +
+  facet_wrap(~continent) +
+  theme(legend.position = "bottom") +
+  # Here comes the gganimate specific bits
+  labs(title = 'Year: {frame_time}', x = 'GDP per capita', y = 'life expectancy') +
+  gganimate::transition_time(year) +
+  gganimate::ease_aes('linear')
+
+
+## ----plot1, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(economics) #<<
+
+
+## ----output1, ref.label="plot1", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot2, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(economics) +
+##   aes(date, unemploy) #<<
+
+
+## ----output2, ref.label="plot2", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot3, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(economics) +
+##   aes(date, unemploy) +
+##   geom_line() #<<
+
+
+## ----output3, ref.label="plot3", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot5-anim, eval=FALSE, echo=TRUE-----------------------------------
+## ggplot(economics) +
+##   aes(date, unemploy) +
+##   geom_line() +
+##   transition_reveal(date) #<<
+
+
+## ----output5-anim, ref.label="plot5-anim", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot5, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(datasaurus_dozen)#<<
+
+
+## ----output5, ref.label="plot5", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot6, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(datasaurus_dozen) +
+##   aes(x, y, color=dataset)#<<
+
+
+## ----output6, ref.label="plot6", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot7, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(datasaurus_dozen) +
+##   aes(x, y, color=dataset) +
+##   geom_point() #<<
+
+
+## ----output7, ref.label="plot7", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot8, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(datasaurus_dozen) +
+##   aes(x, y, color=dataset) +
+##   geom_point() +
+##   facet_wrap(~dataset)#<<
+
+
+## ----output8, ref.label="plot8", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ----plot9, eval=FALSE, echo=TRUE----------------------------------------
+## ggplot(datasaurus_dozen) +
+##   aes(x, y) +
+##   geom_point() +
+##   transition_states(dataset, 3, 1) + #<<
+##   labs(title = "Dataset: {closest_state}") #<<
+## 
+
+
+## ----output9, ref.label="plot9", echo=FALSE, cache=TRUE, fig.height = 6----
+
+
+## ------------------------------------------------------------------------
+library(gapminder)
+
+ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, colour = country)) +
+  geom_point(alpha = 0.7) +
+  scale_colour_manual(values = country_colors, guide=FALSE) +
+  scale_size("Population size", range = c(2, 12), breaks=c(1*10^8, 2*10^8, 5*10^8, 10^9, 2*20^9)) +
+  scale_x_log10() +
+  facet_wrap(~continent) +
+  theme(legend.position = "bottom")
+
