735f38779895ec9b024fd9c1c542a4e0b8df63cb
heike <hofmann@iastate.edu>
2020-07-22 11:30:58 -0500

    export r code

---
+++ code/1.2-ggplot-adv.r
@@ -0,0 +1,127 @@
+## ----echo=FALSE--------------------------------------------------------
+knitr::opts_chunk$set(
+  echo=FALSE,
+  message = FALSE,
+  warning = FALSE,
+  error = FALSE, 
+  collapse = TRUE,
+  comment = "",
+  out.width = "80%", 
+  fig.height = 6,
+  fig.width = 10,
+  fig.align = "center",
+  fig.retina = 3,
+  cache = FALSE
+)
+
+
+## ----load libraries, echo=FALSE----------------------------------------
+library(tidyverse)
+library(ggmap)
+library(here)
+
+
+## ----not to run this code, eval=FALSE----------------------------------
+# Data extracted from ALA with this code
+# install.packages("ALA4R")
+library(ALA4R)
+l <- specieslist("platypus")
+platypus <- occurrences("Ornithorhynchus anatinus", download_reason_id=10)
+save(platypus, file="data/platypus.rda")
+
+
+## ----load the platypus obervation data, echo=TRUE----------------------
+load(here::here("data/platypus.rda"))
+platydata <- platypus$data
+ggplot(data=platydata) + geom_point(aes(x=longitude, y=latitude))
+
+
+## ----Add some transparency to see density of locations, echo=TRUE------
+ggplot(data=platydata) + geom_point(aes(x=longitude, y=latitude), alpha=0.1)
+
+
+## ----making a map projection, echo=TRUE, fig.height=10, out.width="100%"----
+ggplot(data=platydata) + 
+  geom_point(aes(x=longitude, y=latitude), alpha=0.1) +
+  coord_map()
+
+
+## ----you need a developer API to run this code, echo=FALSE, eval=FALSE----
+library(ggmap)
+register_google(XXX)
+oz <- get_map(location=c(lon=133.8807, lat=-23.6980), zoom=4)
+save(oz, file="data/oz.rda")
+
+
+## ----load the saved map data, echo=TRUE--------------------------------
+load(here::here("data/oz.rda"))
+ggmap(oz) + 
+  geom_point(data=platydata, aes(x=longitude, y=latitude), 
+              alpha=0.1, colour="orange")
+
+
+## ----leaflet map, echo=TRUE--------------------------------------------
+library(leaflet)
+platydata %>%
+  filter(!is.na(latitude), !is.na(longitude), !(eventDate=="")) %>%
+  leaflet() %>%
+  addTiles() %>%
+  addCircleMarkers(
+    radius=1, opacity = 0.5, color = "orange", label = ~eventDate,
+    lat = ~latitude, lng = ~longitude) 
+
+
+## ----create a date variable, echo=TRUE---------------------------------
+library(lubridate)
+platydata <- platydata %>% 
+  mutate(eventDate = ymd(eventDate))
+
+
+## ----show sightings ovre time, echo=TRUE, out.width="60%"--------------
+ggplot(data=platydata) +
+  geom_point(aes(x=eventDate, y=1))
+
+
+## ----solution code for filtering on location and time, eval=FALSE------
+# Check odd cases
+platydata %>% filter(latitude < (-50)) 
+# These just have the lat/long wrong
+platydata %>% filter(eventDate < ymd("1850-01-01")) 
+
+
+## ----focus on records since 1900 and count the number for each year, echo=TRUE, out.width="60%"----
+platydata1900 <- platydata %>% filter(year>1900) %>%
+  count(year) 
+ggplot(data=platydata1900) +
+  geom_point(aes(x=year, y=n))
+
+
+## ----add a trend line, echo=TRUE, out.width="60%"----------------------
+ggplot(data=platydata1900, aes(x=year, y=n)) +
+  geom_point() +
+  geom_smooth(se=F)
+
+
+## ----make it interactive to investigate some observations, echo=TRUE----
+library(plotly)
+ggplotly()
+
+
+## ----solution code to density plot on map, eval=FALSE, echo=FALSE------
+ggmap(oz) + 
+  geom_density2d(data=platydata, aes(x=longitude, y=latitude), 
+              colour="orange") 
+
+
+## ----eval=FALSE, echo=FALSE--------------------------------------------
+Not a lot different. It shows perhaps high density regions around Melbourne, Brisbane and south of Sydney. These match high population sites, though, and maybe reiterates the sampling being convenience sampling. We miss the smaller areas of sightings, like northern Queensland.
+
+
+## ----solution code for exploring population trend, echo=FALSE, eval=FALSE----
+platydata_50_10 <- platydata %>% filter(year>1949, year<2010) %>%
+  mutate(decade = cut(year, breaks=seq(1950, 2019, 10),
+   include.lowest=TRUE, 
+   labels=c("50-59", "60-69", "70-79", "80-89", "90-99", "00-09")))
+ggmap(oz) + geom_point(data=platydata_50_10, mapping=aes(x=longitude, y=latitude), colour="orange", alpha=0.1) +
+  facet_wrap(~decade)
+
--- code/1.3-perception.r
+++ code/1.3-perception.r
@@ -1,4 +1,20 @@
-## ----load libraries, echo=FALSE--------------------------------------------------------
+## ----echo=FALSE--------------------------------------------------------
+knitr::opts_chunk$set(
+  echo=FALSE,
+  message = FALSE,
+  warning = FALSE,
+  error = FALSE, 
+  collapse = TRUE,
+  comment = "",
+  fig.height = 6,
+  fig.width = 10,
+  fig.align = "center",
+  fig.retina = 3,
+  cache = FALSE
+)
+
+
+## ----load libraries, echo=FALSE----------------------------------------
 library(tidyverse)
 library(RColorBrewer)
 library(gridExtra)
@@ -6,48 +22,65 @@ library(dichromat)
 library(here)
 
 
-## ----read TB data and wrangle and subset to USA----------------------------------------
-tb <- read_csv(here::here("data/TB_notifications_2019-07-01.csv")) %>%
+## ----read TB data and wrangle and subset to USA------------------------
+tb <- read_csv(here::here("data/TB_notifications_2019-07-01.csv")) %>% 
   select(country, iso3, year, new_sp_m04:new_sp_fu) %>%
   pivot_longer(cols=new_sp_m04:new_sp_fu, names_to="sexage", values_to="count") %>%
   mutate(sexage = str_replace(sexage, "new_sp_", "")) %>%
-  mutate(sex=substr(sexage, 1, 1),
+  mutate(sex=substr(sexage, 1, 1), 
          age=substr(sexage, 2, length(sexage))) %>%
   select(-sexage)
 
 # Filter years between 1997 and 2012 due to missings
-tb_us <- tb %>%
+tb_us <- tb %>% 
   filter(country == "United States of America") %>%
   filter(!(age %in% c("04", "014", "514", "u"))) %>%
   filter(year > 1996, year < 2013)
 
 
-## --------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------
 tb_us %>% filter(year == 2012) %>% select(sex, age, count)
 
 
-## ----focus on one year gender side-by-side bars of males/females, fig.height=3---------
+## ----focus on one year gender side-by-side bars of males/females, fig.height=3----
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=sex, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Arrangement A")
 
 
-## ----focus on one year age side-by-side bars of age group, fig.height=3----------------
+## ----focus on one year age side-by-side bars of age group, fig.height=3----
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=age, y=count, fill=age)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~sex, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Arrangement B")
 
-## --------------------------------------------------------------------------------------
+
+## ----eval=FALSE--------------------------------------------------------
+- Arrangement A makes it easier to directly compare male and female counts, separately for each age group. Generally, male counts are higher than female counts. There is a big difference between counts in the 45-54 age group, and over 65 counts are almost the same.
+- Arrangement B makes it easier to directly compare counts by age group, separately for females and males. For females, incidence drops in the middle years. For males, it is pretty consistently high across age groups. 
+
+
+## ----ref.label='focus on one year gender side-by-side bars of males/females', fig.height=3----
+
+
+## ----ref.label='focus on one year age side-by-side bars of age group', fig.height=3----
+
+
+## ----------------------------------------------------------------------
 tb_us %>% select(year, sex, age, count) %>% head(10)
 
 
-## ----use a line plot instead of bar, fig.height=3--------------------------------------
+## ----eval=FALSE--------------------------------------------------------
+- Plot type A makes it easier to examine trend for each group. This plot should probably have used 0 as the lower limit.
+- Plot type  B is really only allowing the overall trend in count to be examined separately by age. It is also possible to see trend for males. Trend for females is buried because the bars start at irregular heights. The separated bars distract from digesting the overall count. 
+
+
+## ----use a line plot instead of bar, fig.height=3----------------------
 ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   geom_line() + geom_point() +
   facet_wrap(~age, ncol=6) +
@@ -55,17 +88,35 @@ ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   ggtitle("Type A")
 
 
-## ----colour and axes fixes, fig.height=3-----------------------------------------------
+## ----colour and axes fixes, fig.height=3-------------------------------
 # This uses a color blind proof scale
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity") +
+  geom_bar(stat="identity") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Type B")
 
 
-## ----use a line plot for proportions, fig.height=3-------------------------------------
-tb_us %>% group_by(year, age) %>%
+## ----------------------------------------------------------------------
+countdown::countdown(1,50, top=0, right=0)
+
+
+## ----ref.label='use a line plot instead of bar', fig.height=3----------
+
+
+## ----ref.label='colour and axes fixes', fig.height=3-------------------
+
+
+## ----------------------------------------------------------------------
+countdown::countdown(0, 30, top=0, right=0)
+
+
+## ----------------------------------------------------------------------
+countdown::countdown(1, 5, top=100, right=0)
+
+
+## ----use a line plot for proportions, fig.height=3---------------------
+tb_us %>% group_by(year, age) %>% 
   summarise(p = count[sex=="m"]/sum(count)) %>%
   ggplot(aes(x=year, y=p)) +
   geom_hline(yintercept = 0.50, colour="white", size=2) +
@@ -75,17 +126,38 @@ tb_us %>% group_by(year, age) %>%
   ggtitle("Type A")
 
 
-## ----compare proportions of males/females, fig.height=4--------------------------------
+## ----compare proportions of males/females, fig.height=4----------------
 # Fill the bars, note the small change to the code
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="fill") +
+  geom_bar(stat="identity", position="fill") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_brewer("", palette="Dark2") + ylab("proportion") +
   ggtitle("Type B") + theme(legend.position = "bottom")
 
 
-## ----show different types of color palettes, fig.height=7, fig.width=12----------------
+## ----eval=FALSE--------------------------------------------------------
+- Plot A makes it easier to examine the trend in proportion. It is easy to miss that all the proportions are greater than 0.5, despite having a guideline (white) at 0.5. It could be argued that setting the vertical axis limits could alleviate this.  The fluctuations from year to year are more visible. Maybe adding a trend model could be helpful, to reduce this noise. Without colour its less visually appealing.
+- Plot B makes it easier to see that the proportion for males is almost always higher than for females. It also suggests that there is little temporal trend, because the small fluctuations between years is less visible. Having colour makes it more visually appealing. There s less data processing.
+
+
+## ----------------------------------------------------------------------
+countdown::countdown(1, 40, top=100, right=100)
+
+
+## ----eval=FALSE--------------------------------------------------------
+1. scatterplot, barchart
+2. side-by-side boxplot, stacked barchart
+3. piechart, rose plot, gauge plot, donut, wind direction map, starplot
+4. treemap, bubble chart, mosaicplot
+5. chernoff face
+6. choropleth map
+
+
+## ----show different types of color palettes, fig.height=7, fig.width=12, echo=TRUE, fig.show='hide'----
 display.brewer.all()
+
+
+## ----ref.label='show different types of color palettes', fig.height=7, fig.width=12----
 
 
 ## ----mapping numbers to rainbow sequential scale, echo=TRUE, out.width="60%", fig.width=7, fig.height=4----
@@ -106,6 +178,10 @@ d + scale_colour_brewer(palette="PRGn")
 d + scale_colour_brewer(palette="Set1")
 
 
+## ----------------------------------------------------------------------
+countdown::countdown(0, 50, right=50, bottom=0)
+
+
 ## ----using the dichromat package to check color blind appearance, echo=TRUE, fig.show='hide'----
 library(scales)
 library(dichromat)
@@ -115,33 +191,36 @@ clrs <- dichromat(hue_pal()(9))
 d + scale_fill_manual("", values=clrs) + theme(legend.position = "none")
 
 
-## ----show the default colour scheme and dichromat adjusted colors, echo=FALSE, fig.height=8----
+## ----show the default colour scheme, echo=FALSE, fig.width=4, fig.height=4, out.width="100%"----
 clrs <- hue_pal()(9)
 p1 <- d + theme(legend.position = "none") + scale_color_discrete()
+p1
 
+
+## ----show the dichromat adjusted colors, echo=FALSE, fig.width=4, fig.height=4, out.width="100%"----
 clrs <- dichromat(hue_pal()(9))
 p2 <- d + scale_color_manual("", values=clrs) + theme(legend.position = "none")
 
-grid.arrange(p1, p2, ncol=1)
+p2
 
 
-## ----is shape preattentive, echo=FALSE-------------------------------------------------
+## ----is shape preattentive, echo=FALSE---------------------------------
 set.seed(20190715)
 df <- data.frame(x=runif(100), y=runif(100), cl=sample(c(rep("A", 1), rep("B", 99))))
-ggplot(data=df, aes(x, y, shape=cl)) + theme_bw() +
+ggplot(data=df, aes(x, y, shape=cl)) + theme_bw() + 
   geom_point(size=3) +
   theme(legend.position="None", aspect.ratio=1)
 
 
-## ----is color preattentive, echo=FALSE-------------------------------------------------
-ggplot(data=df, aes(x, y, colour=cl)) +
+## ----is color preattentive, echo=FALSE---------------------------------
+ggplot(data=df, aes(x, y, colour=cl)) + 
   geom_point(size=3) +
-  theme_bw() +
+  theme_bw() + 
   scale_colour_brewer(palette="Set1") +
   theme(legend.position="None", aspect.ratio=1)
 
 
-## ----a line plot on sex, fig.height=3--------------------------------------------------
+## ----a line plot on sex, fig.height=3----------------------------------
 ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   geom_line() + geom_point() +
   facet_wrap(~age, ncol=6) +
@@ -150,7 +229,7 @@ ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   ggtitle("Arrangement A")
 
 
-## ----a line plot on age, fig.height=3, fig.width=6-------------------------------------
+## ----a line plot on age, fig.height=3, fig.width=6---------------------
 ggplot(tb_us, aes(x=year, y=count, colour=age)) +
   geom_line() + geom_point() +
   facet_wrap(~sex, ncol=6) +
@@ -159,38 +238,38 @@ ggplot(tb_us, aes(x=year, y=count, colour=age)) +
   ggtitle("Arrangement B")
 
 
-## ----side-by-side bars of males/females, fig.height=3----------------------------------
+## ----side-by-side bars of males/females, fig.height=3------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=sex, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Position - common scale ")
 
 
-## ----piecharts of males/females, fig.height=3------------------------------------------
+## ----piecharts of males/females, fig.height=3--------------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=1, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="fill") +
+  geom_bar(stat="identity", position="fill") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Angle") + xlab("") + ylab("") +
   coord_polar(theta = "y")
 
 
-## ----side-by-side bars of age, fig.height=3--------------------------------------------
+## ----side-by-side bars of age, fig.height=3----------------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=age, y=count, fill=age)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~sex, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Position - common scale ")
 
 
-## ----piecharts of age, fig.height=3----------------------------------------------------
+## ----piecharts of age, fig.height=3------------------------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=1, y=count, fill=age)) +
-  geom_bar(stat="identity", position="fill") +
+  geom_bar(stat="identity", position="fill") + 
   facet_wrap(~sex, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Angle") + xlab("") + ylab("") +
@@ -209,5 +288,9 @@ ggplot(dsamp, aes(x=carat, y=price, colour = clarity)) +
 ggplot(dsamp, aes(x=carat, y=price, colour = clarity)) +
   geom_point() +
   geom_smooth(se=FALSE) +
-  scale_color_brewer(palette="Set1")
+  scale_color_brewer(palette="Set1") 
+
+
+## ----------------------------------------------------------------------
+countdown::countdown(7, 0)
 
