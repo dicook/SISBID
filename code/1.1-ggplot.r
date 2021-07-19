f21bde239fb905d5c0be66af2fb806fa4132ca9d
heike <hofmann@iastate.edu>
2021-07-19 16:29:35 -0500

    update to advanced ggplot2

--- code/1.1-ggplot.r
+++ code/1.1-ggplot.r
@@ -3,10 +3,10 @@ knitr::opts_chunk$set(
   echo=FALSE,
   message = FALSE,
   warning = FALSE,
-  error = FALSE, 
+  error = FALSE,
   collapse = TRUE,
   comment = "",
-  out.width = "80%", 
+  out.width = "80%",
   fig.height = 6,
   fig.width = 10,
   fig.align = "center",
@@ -29,17 +29,17 @@ wordcloud(namedplots, freq = sample(1:10, length(namedplots), replace=TRUE), col
 
 
 ## ----read TB data and wrangle and subset to USA------------------------
-tb <- read_csv(here::here("data/TB_notifications_2020-07-01.csv")) %>% 
+tb <- read_csv(here::here("data/TB_notifications_2020-07-01.csv")) %>%
   select(country, iso3, year, new_sp_m04:new_sp_fu) %>%
   gather(stuff, count, new_sp_m04:new_sp_fu) %>%
   separate(stuff, c("stuff1", "stuff2", "sexage")) %>%
   select(-stuff1, -stuff2) %>%
-  mutate(sex=substr(sexage, 1, 1), 
+  mutate(sex=substr(sexage, 1, 1),
          age=substr(sexage, 2, length(sexage))) %>%
   select(-sexage)
 
 # Filter years between 1997 and 2012 due to missings
-tb_us <- tb %>% 
+tb_us <- tb %>%
   filter(country == "United States of America") %>%
   filter(!(age %in% c("04", "014", "514", "u"))) %>%
   filter(year > 1996, year < 2013) %>%
@@ -51,7 +51,7 @@ tb_us <- tb %>%
 ## ----make a bar chart of US TB incidence, echo=TRUE, out.width="60%", fig.width=10, fig.height=3----
 ggplot(tb_us, aes(x = year, y = count, fill = sex)) +
   geom_bar(stat = "identity") +
-  facet_grid(~ age) 
+  facet_grid(~ age)
 
 
 ## ----eval=FALSE--------------------------------------------------------
@@ -68,22 +68,22 @@ ggplot(tb_us, aes(x = year, y = count, fill = sex)) +
 ## ----colour and axes fixes, echo=TRUE, fig.height=3--------------------
 # This uses a color blind proof scale
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity") + 
+  geom_bar(stat="identity") +
   facet_wrap(~age_group, ncol=6) +
-  scale_fill_brewer("Sex", palette="Dark2") 
+  scale_fill_brewer("Sex", palette="Dark2")
 
 
 ## ----compare proportions of males/females, out.width="70%", echo=TRUE, fig.height=3----
 # Fill the bars, note the small change to the code
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="fill") + 
+  geom_bar(stat="identity", position="fill") +
   facet_wrap(~age_group, ncol=6) +
   scale_fill_brewer("Sex", palette="Dark2") + ylab("proportion")
 
 
 ## ----eval=FALSE--------------------------------------------------------
 - Focus is now on proportions of male and female each year, within age group
-- Proportions are similar across year 
+- Proportions are similar across year
 - Roughly equal proportions at young and old age groups, more male incidence in middle years
 
 
@@ -91,7 +91,7 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
 # This code does something strange to the axis tick marks
 # We will skip it for now
 #ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-#  geom_bar(stat="identity", position="dodge") + 
+#  geom_bar(stat="identity", position="dodge") +
 #  facet_wrap(~age, ncol=6) +
 #  scale_fill_brewer("", palette="Dark2") +
 #  scale_x_continuous("year", breaks=seq(1995, 2012, 5), labels=c("95", "00", "05", "10"))
@@ -100,13 +100,13 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
 ## ----compare counts of males/females, out.width="100%", echo=TRUE, fig.height=5----
 # Make separate plots for males and females, focus on counts by category
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity") + 
+  geom_bar(stat="identity") +
   facet_wrap(sex~age_group, ncol=6) +
-  scale_fill_brewer("", palette="Dark2") 
+  scale_fill_brewer("", palette="Dark2")
 
 
 ## ----eval=FALSE--------------------------------------------------------
-- Counts are generally higher for males than females 
+- Counts are generally higher for males than females
 - There are very few female cases in the middle years
 - Perhaps something of a older male outbreak in 2007-8, and possibly a young female outbreak in the same years
 
@@ -114,7 +114,7 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
 ## ----rose plot of males/females, out.width="60%", echo=TRUE, fig.height=5----
 # How to make a pie instead of a barchart - not straight forward
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity") + 
+  geom_bar(stat="identity") +
   facet_wrap(sex~age_group, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   coord_polar()
@@ -123,34 +123,34 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
 ## ----stacked barchart of males/females, out.width="60%", echo=TRUE, fig.height=5----
 # Step 1 to make the pie
 ggplot(tb_us, aes(x = 1, y = count, fill = factor(year))) +
-  geom_bar(stat="identity", position="fill") + 
+  geom_bar(stat="identity", position="fill") +
   facet_wrap(sex~age_group, ncol=6) +
-  scale_fill_viridis_d("", option="inferno") 
+  scale_fill_viridis_d("", option="inferno")
 
 
 ## ----pie chart of males/females, out.width="60%", echo=TRUE, fig.height=5----
 # Now we have a pie, note the mapping of variables
 # and the modification to the coord_polar
 ggplot(tb_us, aes(x = 1, y = count, fill = factor(year))) +
-  geom_bar(stat="identity", position="fill") + 
+  geom_bar(stat="identity", position="fill") +
   facet_wrap(sex~age_group, ncol=6) +
   scale_fill_viridis_d("", option="inferno") +
-  coord_polar(theta = "y") 
+  coord_polar(theta = "y")
 
 
 ## ----use a line plot instead of bar, fig.height=3----------------------
 ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   geom_line() + geom_point() +
   facet_wrap(~age_group, ncol=6) +
-  scale_colour_brewer("", palette="Dark2") 
+  scale_colour_brewer("", palette="Dark2")
 
 
 ## ----use a line plot of proportions, fig.height=3----------------------
-tb_us %>% group_by(year, age_group) %>% 
+tb_us %>% group_by(year, age_group) %>%
   summarise(p = count[sex=="m"]/sum(count)) %>%
   ggplot(aes(x=year, y=p)) +
   geom_hline(yintercept = 0.50, colour="white", size=2) +
   geom_line() + geom_point() +
   facet_wrap(~age_group, ncol=6) +
-  ylab("proportion of males") 
+  ylab("proportion of males")
 
