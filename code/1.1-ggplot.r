0e25e24d30be8e9fa770edb7019f064314ec7a5f
heike <hofmann@iastate.edu>
2021-07-21 21:57:54 -0500

    packaged up

--- code/1.1-ggplot.r
+++ code/1.1-ggplot.r
@@ -1,4 +1,4 @@
-## ----echo=FALSE-----------------------------------------------------------------
+## ----echo=FALSE------------------------------------------------------------------------
 knitr::opts_chunk$set(
   echo=FALSE,
   message = FALSE,
@@ -15,13 +15,13 @@ knitr::opts_chunk$set(
 )
 
 
-## ----load libraries, echo=FALSE-------------------------------------------------
+## ----load libraries, echo=FALSE--------------------------------------------------------
 library(tidyverse)
 library(ggmap)
 library(here)
 
 
-## ----read TB data and wrangle and subset to USA---------------------------------
+## ----read TB data and wrangle and subset to USA----------------------------------------
 tb <- read_csv(here::here("data/TB_notifications_2020-07-01.csv")) %>% 
   select(country, iso3, year, new_sp_m04:new_sp_fu) %>%
   gather(stuff, count, new_sp_m04:new_sp_fu) %>%
@@ -47,7 +47,7 @@ ggplot(tb_us, aes(x = year, y = count, fill = sex)) +
   facet_grid(~ age) 
 
 
-## ----colour and axes fixes, echo=TRUE, fig.height=3-----------------------------
+## ----colour and axes fixes, echo=TRUE, fig.height=3------------------------------------
 # This uses a color blind friendly scale
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
   geom_bar(stat="identity") + 
@@ -64,7 +64,7 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
   ylab("proportion")
 
 
-## ----side-by-side bars of males/females, fig.height=3, eval=FALSE, echo=FALSE----
+## ----side-by-side bars of males/females, fig.height=3, eval=FALSE, echo=FALSE----------
 ## # This code does something strange to the axis tick marks
 ## # We will skip it for now
 ## # ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
@@ -74,7 +74,7 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
 ## #  scale_x_continuous("year", breaks=seq(1995, 2012, 5), labels=c("95", "00", "05", "10"))
 
 
-## ----compare counts of males/females, out.width="100%", echo=TRUE, fig.height=5----
+## ----compare counts of males/females, out.width="100%", echo=TRUE, fig.height=5--------
 # Make separate plots for males and females, focus on counts by category
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
   geom_bar(stat="identity") + 
@@ -82,13 +82,13 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5")) 
 
 
-## ----eval=FALSE-----------------------------------------------------------------
+## ----eval=FALSE------------------------------------------------------------------------
 ## - Counts are generally higher for males than females
 ## - There are very few female cases in the middle years
 ## - Perhaps something of a older male outbreak in 2007-8, and possibly a young female outbreak in the same years
 
 
-## ----rose plot of males/females, out.width="60%", echo=TRUE, fig.height=5-------
+## ----rose plot of males/females, out.width="60%", echo=TRUE, fig.height=5--------------
 # How to make a pie instead of a barchart - not straight forward
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
   geom_bar(stat="identity") + 
@@ -97,7 +97,7 @@ ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
   coord_polar()
 
 
-## ----stacked barchart of males/females, out.width="60%", echo=TRUE, fig.height=5----
+## ----stacked barchart of males/females, out.width="60%", echo=TRUE, fig.height=5-------
 # Step 1 to make the pie
 ggplot(tb_us, aes(x = 1, y = count, fill = factor(year))) +
   geom_bar(stat="identity", position="fill") + 
@@ -105,7 +105,7 @@ ggplot(tb_us, aes(x = 1, y = count, fill = factor(year))) +
   scale_fill_viridis_d("", option="inferno") 
 
 
-## ----pie chart of males/females, out.width="60%", echo=TRUE, fig.height=5-------
+## ----pie chart of males/females, out.width="60%", echo=TRUE, fig.height=5--------------
 # Now we have a pie, note the mapping of variables
 # and the modification to the coord_polar
 ggplot(tb_us, aes(x = 1, y = count, fill = factor(year))) +
@@ -115,14 +115,14 @@ ggplot(tb_us, aes(x = 1, y = count, fill = factor(year))) +
   coord_polar(theta = "y") 
 
 
-## ----use a line plot instead of bar, fig.height=3-------------------------------
+## ----use a line plot instead of bar, fig.height=3--------------------------------------
 ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   geom_line() + geom_point() +
   facet_wrap(~age_group, ncol=6) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5")) 
 
 
-## ----use a line plot of proportions, fig.height=3-------------------------------
+## ----use a line plot of proportions, fig.height=3--------------------------------------
 tb_us %>% group_by(year, age_group) %>% 
   summarise(p = count[sex=="m"]/sum(count)) %>%
   ggplot(aes(x=year, y=p)) +
