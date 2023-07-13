c0a8a418083514a570bcd2d3c11865e42a499922
dicook <visnut@gmail.com>
2023-07-14 08:45:35 +1000

    materials updates to this year

--- code/1.3-perception.r
+++ code/1.3-perception.r
@@ -1,9 +1,9 @@
-## ----echo=FALSE--------------------------------------------------------------------
+## ----echo=FALSE--------------------------------------------------
 knitr::opts_chunk$set(
   echo=FALSE,
   message = FALSE,
   warning = FALSE,
-  error = FALSE,
+  error = FALSE, 
   collapse = TRUE,
   comment = "",
   fig.height = 6,
@@ -14,7 +14,7 @@ knitr::opts_chunk$set(
 )
 
 
-## ----load libraries, echo=FALSE----------------------------------------------------
+## ----load libraries, echo=FALSE----------------------------------
 library(tidyverse)
 library(RColorBrewer)
 library(gridExtra)
@@ -27,39 +27,40 @@ library(conflicted)
 conflict_prefer("select", "dplyr")
 conflict_prefer("filter", "dplyr")
 
-## ----read TB data and wrangle and subset to USA------------------------------------
-tb <- read_csv(here::here("data/TB_notifications_2019-07-01.csv")) %>%
+
+## ----read TB data and wrangle and subset to USA------------------
+tb <- read_csv(here::here("data/TB_notifications_2019-07-01.csv")) %>% 
   dplyr::select(country, iso3, year, new_sp_m04:new_sp_fu) %>%
   pivot_longer(cols=new_sp_m04:new_sp_fu, names_to="sexage", values_to="count") %>%
   mutate(sexage = str_replace(sexage, "new_sp_", "")) %>%
-  mutate(sex=substr(sexage, 1, 1),
+  mutate(sex=substr(sexage, 1, 1), 
          age=substr(sexage, 2, length(sexage))) %>%
   dplyr::select(-sexage)
 
 # Filter years between 1997 and 2012 due to missings
-tb_us <- tb %>%
+tb_us <- tb %>% 
   filter(country == "United States of America") %>%
   filter(!(age %in% c("04", "014", "514", "u"))) %>%
   filter(year > 1996, year < 2013)
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 tb_us %>% filter(year == 2012) %>% dplyr::select(sex, age, count)
 
 
-## ----focus on one year gender side-by-side bars of males/females, fig.height=3-----
+## ----focus on one year gender side-by-side bars of males/females, fig.height=3----
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=sex, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5")) +
   ggtitle("Arrangement A")
 
 
-## ----focus on one year age side-by-side bars of age group, fig.height=3------------
+## ----focus on one year age side-by-side bars of age group, fig.height=3----
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=age, y=count, fill=age)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~sex, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Arrangement B")
@@ -71,16 +72,16 @@ tb_us %>% filter(year == 2012) %>%
 ## ----ref.label='focus on one year age side-by-side bars of age group', fig.height=3----
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 tb_us %>% select(year, sex, age, count) %>% head(10)
 
 
-## ----eval=FALSE--------------------------------------------------------------------
-#> - Plot type A makes it easier to examine trend for each group. This plot should probably have used 0 as the lower limit.
-#> - Plot type  B is really only allowing the overall trend in count to be examined separately by age. It is also possible to see trend for males. Trend for females is buried because the bars start at irregular heights. The separated bars distract from digesting the overall count.
+## ----eval=FALSE--------------------------------------------------
+## - Plot type A makes it easier to examine trend for each group. This plot should probably have used 0 as the lower limit.
+## - Plot type  B is really only allowing the overall trend in count to be examined separately by age. It is also possible to see trend for males. Trend for females is buried because the bars start at irregular heights. The separated bars distract from digesting the overall count.
 
 
-## ----use a line plot instead of bar, fig.height=3----------------------------------
+## ----use a line plot instead of bar, fig.height=3----------------
 ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   geom_line() + geom_point() +
   facet_wrap(~age, ncol=6) +
@@ -88,35 +89,35 @@ ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   ggtitle("Type A")
 
 
-## ----colour and axes fixes, fig.height=3-------------------------------------------
+## ----colour and axes fixes, fig.height=3-------------------------
 # This uses a color blind proof scale
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity") +
+  geom_bar(stat="identity") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5")) +
   ggtitle("Type B")
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 countdown::countdown(1,50, top=0, right=0)
 
 
-## ----ref.label='use a line plot instead of bar', fig.height=3----------------------
+## ----ref.label='use a line plot instead of bar', fig.height=3----
 
 
-## ----ref.label='colour and axes fixes', fig.height=3-------------------------------
+## ----ref.label='colour and axes fixes', fig.height=3-------------
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 countdown::countdown(0, 30, top=0, right=0)
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 countdown::countdown(1, 5, top=100, right=0)
 
 
-## ----use a line plot for proportions, fig.height=3---------------------------------
-tb_us %>% group_by(year, age) %>%
+## ----use a line plot for proportions, fig.height=3---------------
+tb_us %>% group_by(year, age) %>% 
   summarise(p = count[sex=="m"]/sum(count)) %>%
   ggplot(aes(x=year, y=p)) +
   geom_hline(yintercept = 0.50, colour="white", size=2) +
@@ -126,31 +127,31 @@ tb_us %>% group_by(year, age) %>%
   ggtitle("Type A")
 
 
-## ----compare proportions of males/females, fig.height=4----------------------------
+## ----compare proportions of males/females, fig.height=4----------
 # Fill the bars, note the small change to the code
 ggplot(tb_us, aes(x=year, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="fill") +
+  geom_bar(stat="identity", position="fill") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5")) + ylab("proportion") +
   ggtitle("Type B") + theme(legend.position = "bottom")
 
 
-## ----eval=FALSE--------------------------------------------------------------------
-#> - Plot A makes it easier to examine the trend in proportion. It is easy to miss that all the proportions are greater than 0.5, despite having a guideline (white) at 0.5. It could be argued that setting the vertical axis limits could alleviate this.  The fluctuations from year to year are more visible. Maybe adding a trend model could be helpful, to reduce this noise. Without colour its less visually appealing.
-#> - Plot B makes it easier to see that the proportion for males is almost always higher than for females. It also suggests that there is little temporal trend, because the small fluctuations between years is less visible. Having colour makes it more visually appealing. There s less data processing.
+## ----eval=FALSE--------------------------------------------------
+## - Plot A makes it easier to examine the trend in proportion. It is easy to miss that all the proportions are greater than 0.5, despite having a guideline (white) at 0.5. It could be argued that setting the vertical axis limits could alleviate this.  The fluctuations from year to year are more visible. Maybe adding a trend model could be helpful, to reduce this noise. Without colour its less visually appealing.
+## - Plot B makes it easier to see that the proportion for males is almost always higher than for females. It also suggests that there is little temporal trend, because the small fluctuations between years is less visible. Having colour makes it more visually appealing. There s less data processing.
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 countdown::countdown(1, 40, top=100, right=100)
 
 
-## ----eval=FALSE--------------------------------------------------------------------
-#> 1. scatterplot, barchart
-#> 2. side-by-side boxplot, stacked barchart
-#> 3. piechart, rose plot, gauge plot, donut, wind direction map, starplot
-#> 4. treemap, bubble chart, mosaicplot
-#> 5. chernoff face
-#> 6. choropleth map
+## ----eval=FALSE--------------------------------------------------
+## 1. scatterplot, barchart
+## 2. side-by-side boxplot, stacked barchart
+## 3. piechart, rose plot, gauge plot, donut, wind direction map, starplot
+## 4. treemap, bubble chart, mosaicplot
+## 5. chernoff face
+## 6. choropleth map
 
 
 ## ----show different types of color palettes, fig.height=7, fig.width=12, echo=TRUE, fig.show='hide'----
@@ -178,7 +179,7 @@ d + scale_colour_brewer(palette="PRGn")
 d + scale_colour_brewer(palette="Set1")
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 countdown::countdown(0, 50, right=50, bottom=0)
 
 
@@ -204,23 +205,23 @@ p2 <- d + scale_color_manual("", values=clrs) + theme(legend.position = "none")
 p2
 
 
-## ----is shape preattentive, echo=FALSE---------------------------------------------
+## ----is shape preattentive, echo=FALSE---------------------------
 set.seed(20190715)
 df <- data.frame(x=runif(100), y=runif(100), cl=sample(c(rep("A", 1), rep("B", 99))))
-ggplot(data=df, aes(x, y, shape=cl)) + theme_bw() +
+ggplot(data=df, aes(x, y, shape=cl)) + theme_bw() + 
   geom_point(size=3) +
   theme(legend.position="None", aspect.ratio=1)
 
 
-## ----is color preattentive, echo=FALSE---------------------------------------------
-ggplot(data=df, aes(x, y, colour=cl)) +
+## ----is color preattentive, echo=FALSE---------------------------
+ggplot(data=df, aes(x, y, colour=cl)) + 
   geom_point(size=3) +
-  theme_bw() +
+  theme_bw() + 
   scale_colour_brewer(palette="Set1") +
   theme(legend.position="None", aspect.ratio=1)
 
 
-## ----a line plot on sex, fig.height=3----------------------------------------------
+## ----a line plot on sex, fig.height=3----------------------------
 ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   geom_line() + geom_point() +
   facet_wrap(~age, ncol=6) +
@@ -229,7 +230,7 @@ ggplot(tb_us, aes(x=year, y=count, colour=sex)) +
   ggtitle("Arrangement A")
 
 
-## ----a line plot on age, fig.height=3, fig.width=6---------------------------------
+## ----a line plot on age, fig.height=3, fig.width=6---------------
 ggplot(tb_us, aes(x=year, y=count, colour=age)) +
   geom_line() + geom_point() +
   facet_wrap(~sex, ncol=6) +
@@ -238,48 +239,42 @@ ggplot(tb_us, aes(x=year, y=count, colour=age)) +
   ggtitle("Arrangement B")
 
 
-## ----side-by-side bars of males/females, fig.height=3------------------------------
+## ----side-by-side bars of males/females, fig.height=3------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=sex, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5"))  +
   ggtitle("Position - common scale ")
 
 
-## ----piecharts of males/females, fig.height=3--------------------------------------
+## ----piecharts of males/females, fig.height=3--------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=1, y=count, fill=sex)) +
-  geom_bar(stat="identity", position="fill") +
+  geom_bar(stat="identity", position="fill") + 
   facet_wrap(~age, ncol=6) +
   scale_fill_manual("Sex", values = c("#DC3220", "#005AB5")) +
   ggtitle("Angle") + xlab("") + ylab("") +
-  coord_polar(theta = "y") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) +
-  scale_y_continuous(labels = NULL)
+  coord_polar(theta = "y")
 
 
-## ----side-by-side bars of age, fig.height=3----------------------------------------
+## ----side-by-side bars of age, fig.height=3----------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=age, y=count, fill=age)) +
-  geom_bar(stat="identity", position="dodge") +
+  geom_bar(stat="identity", position="dodge") + 
   facet_wrap(~sex, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Position - common scale ")
 
 
-## ----piecharts of age, fig.height=3------------------------------------------------
+## ----piecharts of age, fig.height=3------------------------------
 tb_us %>% filter(year == 2012) %>%
   ggplot(aes(x=1, y=count, fill=age)) +
-  geom_bar(stat="identity", position="fill") +
+  geom_bar(stat="identity", position="fill") + 
   facet_wrap(~sex, ncol=6) +
   scale_fill_brewer("", palette="Dark2") +
   ggtitle("Angle") + xlab("") + ylab("") +
-  coord_polar(theta = "y") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) +
-  scale_y_continuous(labels = NULL)
+  coord_polar(theta = "y")
 
 
 ## ----facetting plots can result in change blindness, echo=TRUE, out.width="50%", fig.width=6.5, fig.height=3.5----
@@ -294,9 +289,9 @@ ggplot(dsamp, aes(x=carat, y=price, colour = clarity)) +
 ggplot(dsamp, aes(x=carat, y=price, colour = clarity)) +
   geom_point() +
   geom_smooth(se=FALSE) +
-  scale_color_brewer(palette="Set1")
+  scale_color_brewer(palette="Set1") 
 
 
-## ----------------------------------------------------------------------------------
+## ----------------------------------------------------------------
 countdown::countdown(7, 0)
 
--- code/2.4-mvplot.r
+++ code/2.4-mvplot.r
@@ -1,6 +1,6 @@
-## ----echo = FALSE------------------------------------------------------------------
+## ----echo = FALSE------------------------------------------------
 knitr::opts_chunk$set(
-  echo=TRUE,
+  echo=TRUE, 
   message = FALSE,
   warning = FALSE,
   error = FALSE,
@@ -14,7 +14,7 @@ knitr::opts_chunk$set(
 )
 
 
-## ----echo=FALSE--------------------------------------------------------------------
+## ----echo=FALSE--------------------------------------------------
 library(tidyverse)
 library(lubridate)
 library(GGally)
@@ -26,31 +26,31 @@ library(plotly)
 library(palmerpenguins)
 
 
-## ----echo=FALSE--------------------------------------------------------------------
+## ----echo=FALSE--------------------------------------------------
 countdown::countdown(1,0)
 
 
-## ----scatterplot matrix, fig.show='hide'-------------------------------------------
+## ----scatterplot matrix, fig.show='hide'-------------------------
 # Make a simple scatterplot matrix of the new penguins data
-penguins <- penguins %>% filter(!is.na(bill_length_mm))
+penguins <- penguins %>% filter(!is.na(bill_length_mm)) 
 ggpairs(penguins, columns=c(3:6))
 
 
-## ----ref.label="scatterplot matrix", echo=FALSE, fig.width=6, fig.height=6---------
+## ----ref.label="scatterplot matrix", echo=FALSE, fig.width=6, fig.height=6----
 
 
-## ----scatterplot matrix with colour, echo=TRUE, fig.show='hide'--------------------
+## ----scatterplot matrix with colour, echo=TRUE, fig.show='hide'----
 # Re-make mapping colour to species (class)
-ggpairs(penguins, columns=c(3:6),
+ggpairs(penguins, columns=c(3:6), 
         ggplot2::aes(colour=species))
 
 
 ## ----ref.label="scatterplot matrix with colour", echo=FALSE, fig.width=6, fig.height=6----
 
 
-## ----correlation heatmap, echo=TRUE, fig.show='hide'-------------------------------
+## ----correlation heatmap, echo=TRUE, fig.show='hide'-------------
 # Look at one species only
-adelie <- penguins %>%
+adelie <- penguins %>% 
   filter(species == "Adelie") %>%
   select(bill_length_mm:body_mass_g)
 ggcorr(adelie)
@@ -59,34 +59,34 @@ ggcorr(adelie)
 ## ----ref.label="correlation heatmap", echo=FALSE, fig.width=4, fig.height=4, out.width="90%"----
 
 
-## ----corrgram, echo=TRUE, fig.show='hide'------------------------------------------
+## ----corrgram, echo=TRUE, fig.show='hide'------------------------
 # install.packages("corrgram")
 library(corrgram)
-corrgram(adelie,
+corrgram(adelie, 
   lower.panel=corrgram::panel.ellipse)
 
 
 ## ----ref.label="corrgram", echo=FALSE, fig.width=4, fig.height=4, out.width="90%"----
 
 
-## ----generalised pairs plot, fig.show='hide'---------------------------------------
+## ----generalised pairs plot, fig.show='hide'---------------------
 # Matrix plot when variables are not numeric
 data(australia_PISA2012)
 australia_PISA2012 <- australia_PISA2012 %>%
-  mutate(desk = factor(desk),
+  mutate(desk = factor(desk), 
          room = factor(room),
-         study = factor(study),
+         study = factor(study), 
          computer = factor(computer),
-         software = factor(software),
+         software = factor(software), 
          internet = factor(internet),
-         literature = factor(literature),
+         literature = factor(literature), 
          poetry= factor(poetry),
-         art = factor(art),
+         art = factor(art), 
          textbook = factor(textbook),
          dictionary = factor(dictionary),
          dishwasher = factor(dishwasher))
-australia_PISA2012 %>%
-  filter(!is.na(dishwasher)) %>%
+australia_PISA2012 %>% 
+  filter(!is.na(dishwasher)) %>% 
   ggpairs(columns=c(3, 15, 16, 21, 26))
 
 
@@ -95,111 +95,96 @@ australia_PISA2012 %>%
 
 ## ----generalised pairs plot enhance plots, echo=TRUE, fig.width=6, fig.height=6----
 # Modify the defaults, set the transparency of points since there is a lot of data
-australia_PISA2012 %>%
-  filter(!is.na(dishwasher)) %>%
-  ggpairs(columns=c(3, 15, 16, 21, 26),
+australia_PISA2012 %>% 
+  filter(!is.na(dishwasher)) %>% 
+  ggpairs(columns=c(3, 15, 16, 21, 26), 
           lower = list(continuous = wrap("points", alpha=0.05)))
 
 
-## ----design own plot function------------------------------------------------------
+## ----design own plot function------------------------------------
 # Make a special style of plot to put in the matrix
 my_fn <- function(data, mapping, method="loess", ...){
-      p <- ggplot(data = data, mapping = mapping) +
-      geom_point(alpha=0.2, size=1) +
+      p <- ggplot(data = data, mapping = mapping) + 
+      geom_point(alpha=0.2, size=1) + 
       geom_smooth(method="lm", ...)
       p
 }
 
 
-<<<<<<< HEAD
 ## ----generalised pairs plot enhance more, echo=TRUE, fig.show='hide'----
-australia_PISA2012 %>%
-  filter(!is.na(dishwasher)) %>%
-  ggpairs(columns=c(3, 15, 16, 21, 26),
-=======
-## ----generalised pairs plot enhance more, echo=TRUE, fig.show='hide'---------------
 australia_PISA2012 %>% 
   filter(!is.na(dishwasher)) %>% 
   ggpairs(columns=c(3, 15, 16, 21, 26), 
->>>>>>> 557476b23d4e671565631f0ee331a46541490bf1
           lower = list(continuous = my_fn))
 
 
 
 ## ----ref.label='generalised pairs plot enhance more', echo=FALSE, fig.width=6, fig.height=6, outwidth="90%"----
-australia_PISA2012 %>%
-  filter(!is.na(dishwasher)) %>%
-  ggpairs(columns=c(3, 15, 16, 21, 26),
+australia_PISA2012 %>% 
+  filter(!is.na(dishwasher)) %>% 
+  ggpairs(columns=c(3, 15, 16, 21, 26), 
           lower = list(continuous = my_fn))
 
 
 
-## ----echo=FALSE--------------------------------------------------------------------
+## ----echo=FALSE--------------------------------------------------
 countdown::countdown(8,0)
 
 
-<<<<<<< HEAD
-## ----echo=FALSE, eval=FALSE-------------------------------
-australia_PISA2012 %>%
-   filter(!is.na(dishwasher)) %>%
-   mutate(dishwasher = factor(dishwasher)) %>%
-   ggpairs(columns=c(3, 15, 16, 21, 26),
-           lower = list(combo = "box_no_facet"),
-           upper = list(continuous = "density",
-                        combo = "facet_density"))
-=======
-## ----echo=FALSE, eval=FALSE--------------------------------------------------------
-#> australia_PISA2012 %>%
-#>   filter(!is.na(dishwasher)) %>%
-#>   ggpairs(columns=c(3, 15, 16, 21, 26),
-#>           lower = list(combo = "box_no_facet"),
-#>           upper = list(continuous = "density"))
->>>>>>> 557476b23d4e671565631f0ee331a46541490bf1
+## ----echo=FALSE, eval=FALSE--------------------------------------
+## australia_PISA2012 %>%
+##   filter(!is.na(dishwasher)) %>%
+##   ggpairs(columns=c(3, 15, 16, 21, 26),
+##           lower = list(combo = "box_no_facet"),
+##           upper = list(continuous = "density"))
 
 
-## ----wrangle housing data----------------------------------------------------------
+## ----wrangle housing data----------------------------------------
 housing <- read_csv(here::here("data/housing.csv")) %>%
   mutate(date = dmy(date)) %>%
   mutate(year = year(date)) %>%
   filter(year == 2016) %>%
   filter(!is.na(bedroom2), !is.na(price)) %>%
   filter(bedroom2 < 7, bathroom < 5) %>%
-  mutate(bedroom2 = factor(bedroom2),
-         bathroom = factor(bathroom))
+  mutate(bedroom2 = factor(bedroom2), 
+         bathroom = factor(bathroom)) 
 
 
 ## ----make a regression style pairs plot, out.width="100%", fig.width=8, fig.height=3----
-ggduo(housing[, c(4,3,8,10,11)],
-      columnsX = 2:5, columnsY = 1,
-      aes(colour=type, fill=type),
-      types = list(continuous =
-                     wrap("smooth",
+ggduo(housing[, c(4,3,8,10,11)], 
+      columnsX = 2:5, columnsY = 1, 
+      aes(colour=type, fill=type), 
+      types = list(continuous = 
+                     wrap("smooth", 
                        alpha = 0.10)))
 
 
-## ----eval=FALSE, echo=FALSE--------------------------------------------------------
-#> if (!requireNamespace("BiocManager", quietly = TRUE))
-#>     install.packages("BiocManager")
-#> BiocManager::install("bigPint")
+## ----eval=FALSE, echo=FALSE--------------------------------------
+## if (!requireNamespace("BiocManager", quietly = TRUE))
+##     install.packages("BiocManager")
+## BiocManager::install("bigPint")
 
 
-## ----soybean_scat, fig.show='hide'-------------------------------------------------
-library(bigPint) # BiocManager::install("bigPint")
+## ----soybean_scat, fig.show='hide'-------------------------------
+# if (!require("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+# BiocManager::install("bigPint")
+library(bigPint) 
 data(soybean_ir_sub)
 soybean_ir_sub[,-1] <- log(soybean_ir_sub[,-1]+1)
-ggplot(soybean_ir_sub,
-       aes(x=N.1, y=P.1)) +
-  geom_point() +
+ggplot(soybean_ir_sub, 
+       aes(x=N.1, y=P.1)) + 
+  geom_point() + 
   theme(aspect.ratio=1)
 
 
 ## ----ref.label='soybean_scat', fig.width=4, fig.height=4, out.width="90%", echo=FALSE----
 
 
-## ----soybean_litre, fig.show='hide'------------------------------------------------
+## ----soybean_litre, fig.show='hide'------------------------------
 geneList = soybean_ir_sub_metrics[["N_P"]][1:5,]$ID
-ret <- plotLitre(data = soybean_ir_sub,
-                 geneList = geneList,
+ret <- plotLitre(data = soybean_ir_sub, 
+                 geneList = geneList, 
                  pointColor = "deeppink")
 names(ret)
 ret[["N_P_Glyma.19G168700.Wm82.a2.v1"]]
@@ -208,21 +193,12 @@ ret[["N_P_Glyma.19G168700.Wm82.a2.v1"]]
 ## ----ref.label='soybean_litre', fig.width=6, fig.height=6, out.width="90%", echo=FALSE, results='hide'----
 
 
-<<<<<<< HEAD
-## ----soybean_litre_sm, fig.show='hide'--------------------
-ret <- plotSM(soybean_cn_sub,
-              soybean_cn_sub_metrics,
-              option = "hexagon",
-              xbins = 5,
-              pointSize = 0.1,
-=======
-## ----soybean_litre_sm, fig.show='hide'---------------------------------------------
+## ----soybean_litre_sm, fig.show='hide'---------------------------
 ret <- plotSM(soybean_cn_sub, 
               soybean_cn_sub_metrics, 
               option = "hexagon", 
               xbins = 5, 
               pointSize = 0.1, 
->>>>>>> 557476b23d4e671565631f0ee331a46541490bf1
               saveFile = FALSE)
 ret[[2]]
 
@@ -230,17 +206,10 @@ ret[[2]]
 ## ----ref.label='soybean_litre_sm', fig.width=6, fig.height=6, out.width="110%", echo=FALSE, results='hide'----
 
 
-<<<<<<< HEAD
-## ----soybean_pcp, fig.show='hide'-------------------------
-ret <- plotPCP(data = soybean_ir_sub,
-               geneList = geneList,
-               lineSize = 0.3,
-=======
-## ----soybean_pcp, fig.show='hide'--------------------------------------------------
+## ----soybean_pcp, fig.show='hide'--------------------------------
 ret <- plotPCP(data = soybean_ir_sub, 
                geneList = geneList, 
                lineSize = 0.3, 
->>>>>>> 557476b23d4e671565631f0ee331a46541490bf1
                saveFile = FALSE)
 ret[[1]]
 
