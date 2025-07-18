d832cbb7274caebf552ce6052bfe0642819e9ccb
dicook <visnut@gmail.com>
2025-07-18 11:17:51 +1000

    streamlined library loading - all in one file now

---
+++ libraries.R
@@ -0,0 +1,72 @@
+#library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(ggmap)
+library(here)
+library(lubridate)
+library(stringr)
+library(ggbeeswarm)
+library(leaflet)
+library(plotly)
+library(RColorBrewer)
+library(gridExtra)
+library(scales)
+library(dichromat)
+library(broom)
+library(broom.mixed)
+library(lme4)
+library(ggpcp)
+library(colorspace)
+library(corrgram)
+library(palmerpenguins)
+library(GGally)
+library(htmltools)
+library(tourr)
+library(viridis)
+library(nullabor)
+library(forecast)
+library(readxl)
+library(ggthemes)
+library(maps)
+library(splitstackshape)
+library(MASS)
+library(gganimate)
+library(datasauRus)
+library(gapminder)
+library(cranlogs)
+library(shiny)
+library(bslib)
+library(DT)
+
+library(remotes)
+# remotes::install_github("wmurphyrd/fiftystater")
+library(fiftystater)
+# remotes::install_github("heike/vinference")
+library(vinference)
+
+# For slides
+library(xaringan)
+library(xaringanExtra)
+xaringanExtra::use_xaringan_extra(
+  include = c("panelset")
+)
+
+library(conflicted)
+conflicts_prefer(dplyr::filter)
+conflicts_prefer(dplyr::select)
+conflicts_prefer(dplyr::slice)
+conflicts_prefer(dplyr::rename)
+conflicts_prefer(palmerpenguins::penguins)
+
+# Better formatted penguins data
+stdd <- function(x) (x-mean(x))/sd(x)
+penguins_std <- penguins |>
+  filter(!is.na(bill_length_mm)) |>
+  rename(bl = bill_length_mm,
+         bd = bill_depth_mm,
+         fl = flipper_length_mm,
+         bm = body_mass_g) |>
+  mutate_at(vars(bl:bm), stdd) |>
+  select(species, bl:bm)
