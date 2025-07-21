df79185219eff6b20c24066249cc2eef33c7622c
Susan Vanderplas <srvanderplas@gmail.com>
2025-07-21 12:48:53 -0500

    Put code for setup into separate files and reference so they're included in the about/README/index doc

---
+++ r-pkg-deps.R
@@ -0,0 +1,20 @@
+# CRAN packages
+packages <- c(
+  "here", "readr", "readxl", "splitstackshape", "tidyr", "dplyr", "lubridate",
+  "stringr", "purrr", "ggplot2", "ggthemes", "RColorBrewer", "scales",
+  "dichromat", "colorspace", "viridis", "ggbeeswarm", "ggmap", "gridExtra",
+  "GGally", "ggpcp", "corrgram", "tourr", "gganimate", "maps","datasauRus",
+  "gapminder", "cranlogs", "shiny", "bslib", "DT", "leaflet", "plotly",
+  "htmltools", "broom", "broom.mixed", "lme4", "MASS", "forecast", "nullabor")
+
+# Install packages and their dependencies
+install.packages(packages, dep=TRUE, repos = "https://cloud.r-project.org/")
+
+# Install some packages from GitHub
+remotes::install_github("wmurphyrd/fiftystater")
+remotes::install_github("heike/vinference")
+
+# For sharing web apps,
+# but you need adminstrator rights to your computer:
+
+install.packages("rsconnect")
