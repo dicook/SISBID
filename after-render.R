a6d8a8d606d8bc05a3739ddb562ffd93a5cad039
Susan Vanderplas <srvanderplas@gmail.com>
2025-07-23 10:47:41 -0500

    Updates - rerender and provide automatic fixing of README.md and slides/html/lib locations required for everything to work properly

---
+++ after-render.R
@@ -0,0 +1,20 @@
+#!/usr/bin/R
+
+library(here)
+
+readme_md = here("docs", "README.md")
+
+if(file.exists(readme_md)) {
+  file.copy(readme_md, here())
+}
+
+html_lib = here("slides", "html", "lib")
+if(dir.exists(html_lib)) {
+  html_lib_files = list.files(html_lib, all.files=T, full.names = T, recursive = T)
+  html_lib_new = gsub(
+    html_lib,
+    here("docs", "slides", "html", "lib"),
+    html_lib_files, fixed=T)
+    dir_created <- sapply(unique(dirname(html_lib_new)), dir.create, recursive = T)
+    file.copy(html_lib_files, html_lib_new, overwrite=T, recursive = T)
+}
--- after-render.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Move compiled readme in gfm format to home directory
-# so it shows up on project page
-cp docs/README.md .
-# Move html/lib files to docs so that plotly stuff renders properly
-cp -r slides/html/lib docs/slides/html
