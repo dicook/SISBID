207bf85887c4715a1b6c8c037b3a16b28b48842f
Susan Vanderplas <srvanderplas@gmail.com>
2025-07-24 08:18:05 -0500

    Updates - a footer with a link back to the main page.

--- docs/r-pkg-deps.R
+++ docs/r-pkg-deps.R
@@ -6,18 +6,20 @@ packages <- c(
   "GGally", "ggpcp", "corrgram", "tourr", "gganimate", "maps","datasauRus",
   "gapminder", "cranlogs", "shiny", "bslib", "DT", "leaflet", "plotly",
   "htmltools", "broom", "broom.mixed", "lme4", "MASS", "forecast", "nullabor",
-  "ggdist", "bsicons", "ragg", "showtext", "thematic", "remotes", "quarto")
+  "ggdist", "bsicons", "ragg", "showtext", "thematic", "remotes", "quarto",
+  "superheat", "bslib")
 
 # Install packages and their dependencies
 to_install <- setdiff(packages, installed.packages())
 install.packages(to_install, dep=TRUE, repos = "https://cloud.r-project.org/")
 
 # Install some packages from GitHub
+# If you can't install these,
+# it won't affect your ability to participate
 remotes::install_github("wmurphyrd/fiftystater")
 remotes::install_github("heike/vinference")
 remotes::install_github("rstudio/bslib")
 
 # For sharing web apps,
-# but you need adminstrator rights to your computer:
-
+# but you need administrator rights to your computer:
 install.packages("rsconnect")
