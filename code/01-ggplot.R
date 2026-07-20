f46c132be98fe887be476429daacc878231a282f
Heike Hofmann <hhofmann4@unl.edu>
2026-07-19 22:22:23 -0500

    link to TB data for 2026

--- code/01-ggplot.R
+++ code/01-ggplot.R
@@ -9,7 +9,7 @@ source("../libraries.R")
 ## -----------------------------------------------------------------------------
 #| label: read-and-wrangle-TB-data
 #| echo: false
-tb <- readr::read_csv("../data/TB_notifications_2025-07-22.csv") |>
+tb <- readr::read_csv("../data/TB_notifications_2026-07-20.csv") |>
   dplyr::select(country, iso3, year, new_sp_m04:new_sp_fu) |>
   pivot_longer(new_sp_m04:new_sp_fu, names_to="stuff", values_to="count") |>
   separate(stuff, c("stuff1", "stuff2", "sexage")) |>
